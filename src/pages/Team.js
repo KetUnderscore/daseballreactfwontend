c78b73ff2250086893136ccb34024dac01a8fb93
KetUnderscore <tannersock14@gmail.com>
2024-08-10 09:25:22 -0700

    Mods

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -207,6 +207,7 @@ function Team() {
                                                     { item.name === "Unlucky" ? <img src="https://i.imgur.com/sTrsKvH.png" height="20px" title="This player just can't catch a break!"></img> : '' }
                                                     { item.name === "Early Bird" ? <img src="https://i.imgur.com/4OKwfNk.png" height="20px" title="This player draws a walk on three balls."></img> : '' }
                                                     { item.name === "Multiple" ? <img src="https://i.imgur.com/P9EWUY3.png" height="20px" title="This player scores twice."></img> : '' }
+                                                    { item.name === "Siren" ? <img src="https://i.imgur.com/3CsxZ4f.png" height="20px" title="This player has an alluring voice."></img> : '' }
             
                                                     { item.name === "Shepard" ? <img src="https://i.imgur.com/zm2KgpM.png" height="20px" title="This player plays better for every player on base."></img> : '' }
                                                     { item.name === "Sunsetter" ? <img src="https://i.imgur.com/nsGvwgp.png" height="20px" title="This player plays better later in the game."></img> : '' }
@@ -218,6 +219,8 @@ function Team() {
                                                     { item.name === "HeatingUp" ? <img src="https://i.imgur.com/wWxWKyI.png" height="20px" title="This player is heating up..."></img> : '' }
                                                     { item.name === "On Fire" ? <img src="https://i.imgur.com/N1MvyL6.png" height="20px" title="This player is on fire!"></img> : '' }
                                                     { item.name === "Burnt Out" ? <img src="https://i.imgur.com/qmw52U7.png" height="20px" title="This player is burnt out..."></img> : '' }
+                                                    { item.name === "Wired" ? <img src="https://i.imgur.com/he4j9B0.png" height="30px" title="This player is wired!"></img> : '' }
+                                                    { item.name === "Tired" ? <img src="https://i.imgur.com/OFRRbPE.png" height="30px" title="This player is tired..."></img> : '' }
                                                     </>
                                                 )
                                             }) : ''}
@@ -244,7 +247,7 @@ function Team() {
                                             {playerHiddenData != true ? "★".repeat(Math.max(0, Math.floor(statstotal / 400))) + (statstotal % 400 >= 200 ? '☆' : '') : ''}
                                             {playerHiddenData != false ? item.modifiers.map( (item) => {
                                                 return (
-                                                    <> 
+                                                    <>
                                                         { item.name === "Visitor" ? <img src="https://i.imgur.com/ZjehUbe.png" height="20px" title="This player is a visitor from a far off land."></img> : '' }
                                                         { item.name === "Hlockey" ? <img src="https://i.imgur.com/kUcBFL9.png" height="20px" title="This player plays in Hlockey. This player has improved Assault."></img> : '' }
                                                         { item.name === "TerrorBall" ? <img src="https://i.imgur.com/iRVkDQU.png" height="20px" title="This player plays in Terror Ball. This player has improved Return Of The Killer Tomatoes 2."></img> : '' }
@@ -262,6 +265,7 @@ function Team() {
                                                         { item.name === "Unlucky" ? <img src="https://i.imgur.com/sTrsKvH.png" height="20px" title="This player just can't catch a break!"></img> : '' }
                                                         { item.name === "Early Bird" ? <img src="https://i.imgur.com/4OKwfNk.png" height="20px" title="This player draws a walk on three balls."></img> : '' }
                                                         { item.name === "Multiple" ? <img src="https://i.imgur.com/P9EWUY3.png" height="20px" title="This player scores twice."></img> : '' }
+                                                        { item.name === "Siren" ? <img src="https://i.imgur.com/3CsxZ4f.png" height="20px" title="This player has an alluring voice."></img> : '' }
                 
                                                         { item.name === "Shepard" ? <img src="https://i.imgur.com/zm2KgpM.png" height="20px" title="This player plays better for every player on base."></img> : '' }
                                                         { item.name === "Sunsetter" ? <img src="https://i.imgur.com/nsGvwgp.png" height="20px" title="This player plays better later in the game."></img> : '' }
@@ -273,6 +277,8 @@ function Team() {
                                                         { item.name === "HeatingUp" ? <img src="https://i.imgur.com/wWxWKyI.png" height="20px" title="This player is heating up..."></img> : '' }
                                                         { item.name === "On Fire" ? <img src="https://i.imgur.com/N1MvyL6.png" height="20px" title="This player is on fire!"></img> : '' }
                                                         { item.name === "Burnt Out" ? <img src="https://i.imgur.com/qmw52U7.png" height="20px" title="This player is burnt out..."></img> : '' }
+                                                        { item.name === "Wired" ? <img src="https://i.imgur.com/he4j9B0.png" height="30px" title="This player is wired!"></img> : '' }
+                                                        { item.name === "Tired" ? <img src="https://i.imgur.com/OFRRbPE.png" height="30px" title="This player is tired..."></img> : '' }
                                                     </>
                                                 )
                                             }) : ''}
