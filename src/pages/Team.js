2b9fe124e2e8a6a00fb983ca05caecacb712bd12
KetUnderscore <tannersock14@gmail.com>
2024-11-10 20:59:32 -0800

    Whoops, import

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import ProgressBar from './ProgressBar'
 
 function Team() {
     const params = useParams()
