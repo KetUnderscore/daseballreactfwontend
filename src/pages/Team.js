eadd5c734186b50750952a104d65abf7037f3c05
KetUnderscore <tannersock14@gmail.com>
2024-11-10 21:07:01 -0800

    One last try to keep this progress bar

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import ProgressBar from './components/ProgressBar'
+import ProgressBar from '../components/ProgressBar.js'
 
 function Team() {
     const params = useParams()
