576974f134196a9282e5e3ea4453a79b2ab3133b
KetUnderscore <tannersock14@gmail.com>
2024-11-10 21:33:19 -0800

    After all that fiddling I decided against the progress bar

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -111,8 +111,7 @@ function Team() {
                                         teamData[0].curVibe >= 26 ? "↑↑↑↑ Impeccable" :
                                         "None"
                                     }</h1>
-                        <h1>Spirit Fund</h1>
-                        <div style={{display: 'flex', justifyContent: 'center'}}><ProgressBar bgcolor={progbar.bgcolor} completed={progbar.completed} /></div>
+                        <h1>Spirit Fund | {teamData[0].spiritFund }/ 10000</h1>
                         <h1>W { teamData[0].gamesWon } / L { teamData[0].gamesLost }</h1>
 
                         <div className="center row">
