b0483679dc08c4ff586dd4afb44fd55622239396
KetUnderscore <tannersock14@gmail.com>
2024-11-16 20:13:33 -0800

    Fixed vibe read

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -101,15 +101,15 @@ function Team() {
                     <div className="player">
                         <h1>{ teamData[0].teamEmoji }{ teamData[0].teamName }</h1>
                         <h1>W { teamData[0].gamesWon } / L { teamData[0].gamesLost }</h1>
-                        <h2>Vibes | {   teamData[0].curVibe < -25 ? "↓↓↓↓ Devastating" :
-                                        teamData[0].curVibe < -15 ? "↓↓↓ Horrid" :
-                                        teamData[0].curVibe < -5 ?  "↓↓ Bad" :
-                                        teamData[0].curVibe < 0 ?   "↓ Not Great" :
-                                        teamData[0].curVibe < 1 ?   "⟷ Meh" :
-                                        teamData[0].curVibe < 6 ?   "↑ Pretty Good" :
-                                        teamData[0].curVibe < 16 ?  "↑↑ Great" :
-                                        teamData[0].curVibe < 26 ?  "↑↑↑ Amazing" :
-                                        teamData[0].curVibe >= 26 ? "↑↑↑↑ Impeccable" :
+                        <h2>Vibes | {   teamData[0].curVibe < -24 ? "↓↓↓↓ Devastating" :
+                                        teamData[0].curVibe > -25 && teamData[0].curVibe < -16 ? "↓↓↓ Horrid" :
+                                        teamData[0].curVibe > -15 && teamData[0].curVibe < -5 ?  "↓↓ Bad" :
+                                        teamData[0].curVibe > -6 && teamData[0].curVibe < 0 ?   "↓ Not Great" :
+                                        teamData[0].curVibe === 1 ?   "⟷ Meh" :
+                                        teamData[0].curVibe > 0 && teamData[0].curVibe < 6 ?   "↑ Pretty Good" :
+                                        teamData[0].curVibe > 5 && teamData[0].curVibe < 16 ?  "↑↑ Great" :
+                                        teamData[0].curVibe > 15 && teamData[0].curVibe < 26 ?  "↑↑↑ Amazing" :
+                                        teamData[0].curVibe > 25 ? "↑↑↑↑ Impeccable" :
                                         "None"
                                     }</h2>
                         <h2>Spirit Fund | {teamData[0].spiritFund } / 10000</h2>
