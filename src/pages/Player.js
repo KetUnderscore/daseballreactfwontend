29e4d8f78695473f6e2735ab0bfec77c85f97297
KetUnderscore <124571979+KetUnderscore@users.noreply.github.com>
2025-01-27 15:47:25 -0800

    Update Player.js to have season 7

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -10,7 +10,7 @@ function Player () {
     const [playerStatData, setPlayerStatData] = useState(null)
     const [playerLoaded, setPlayerLoaded] = useState(false)
     const [playerStatsLoaded, setPlayerStatsLoaded] = useState(false)
-    const [seasonNumber, setSeasonNumber] = useState(6) // Default is Season 6
+    const [seasonNumber, setSeasonNumber] = useState(7) // Default is Season 7
     
     let favoriteSeason
     let favoritePosition
@@ -43,6 +43,7 @@ function Player () {
                     <option value="4.2">4.2</option>
                     <option value="5">5</option>
                     <option value="6">6</option>
+                    <option value="7">7</option>
                 </select>
             </div>
         )
