fda1d48c96f889b8e878e39d7024c4ad112ba00c
KetUnderscore <tannersock14@gmail.com>
2025-01-29 16:47:50 -0800

    what is life

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -43,6 +43,7 @@ const Login = () => {
             })
             // Reset States
             console.log(response)
+            console.log(response.userInfo)
             setuserDataUsed(response.data)
             setUser('')
             setPwd('')
