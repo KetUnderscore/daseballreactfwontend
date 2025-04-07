70ad80312d8b1379682d02d75384e9581c4c30d3
KetUnderscore <tannersock14@gmail.com>
2025-04-07 10:13:40 -0700

    Binder image?

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -62,19 +62,15 @@ function Games() {
                                                 <h3 className='center'>Day {item.gameDay} Game {item.gameNumber}</h3>
                                                 <h3 className='split-para-old'>
                                                     {
-                                                        item.weather === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="30px" class="weather" title="Null" alt='Null'></img> : ''
+                                                        item.weather === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="30px" class="weather" title="Null" alt='Null'></img>
+                                                        <img src="https://i.imgur.com/2pd8zq8.png" height="30px" class="weather" title="Binder" alt='Binder'></img> : ''
                                                     }
                                                     {
-                                                        item.weather === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="30px" class="weather" title="Sunny" alt='Sunny'></img> : ''
-                                                    }
-                                                    {
-                                                        item.weather === 1 ? <img src="https://i.imgur.com/2pd8zq8.png" height="30px" class="weather" title="Binder" alt='Binder'></img> : ''
+                                                        item.weather === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="30px" class="weather" title="Sunny" alt='Sunny'></img>
+                                                        <img src="https://i.imgur.com/2pd8zq8.png" height="30px" class="weather" title="Binder" alt='Binder'></img> : ''
                                                     }
                                                     {
                                                         item.weather === 2 ? <img src="https://i.imgur.com/K3DWIqZ.png" height="30px" class="weather" title="Shuffle" alt='Shuffle'></img> : ''
-                                                    }
-                                                    {
-                                                        item.weather === 2 ? <img src="https://i.imgur.com/2pd8zq8.png" height="30px" class="weather" title="Binder" alt='Binder'></img> : ''
                                                     }
                                                     {
                                                         item.weather === 3 ? <img src="https://i.imgur.com/YDH7LQT.png" height="30px" class="weather" title="Waves" alt='Waves'></img> : ''
