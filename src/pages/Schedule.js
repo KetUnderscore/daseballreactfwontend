2698c0abc3da91b6c8f5bdfc1fca838d2ac4f055
KetUnderscore <tannersock14@gmail.com>
2025-04-12 09:59:44 -0700

    postseasonScheduleFix

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -88,54 +88,80 @@ function Schedule() {
     }
 
     const loadDays = async () => {
-        if (seasonData != null && seasonData[0].scheduleTeamInfo.length > 0) {
+        if (seasonData[0] != null && seasonData[0] != undefined && seasonData[0] != [] && seasonData[0].scheduleTeamInfo.length > 0) {
             setDaysLoaded(true)
             let teamLength = 12
             let teamSlotA = 0
             let teamSlotB = 0
-            if (seasonData[0].seasonDay >= 45) {if (seasonData[0].scheduleTeamInfo[seasonData[0].seasonDay-45].length == 2) {teamLength = 2} else {teamLength = 4}}
-            for (let daysLeft = seasonData[0].seasonDay; daysLeft < seasonData[0].schedule.length; daysLeft++) {
-                for (let gn = 0; gn < teamLength; gn += 2) {
-                    let team1 = seasonData[0].scheduleTeamInfo[seasonData[0].schedule[daysLeft][gn]]
-                    let team2 = seasonData[0].scheduleTeamInfo[seasonData[0].schedule[daysLeft][gn+1]]
-                    setDayOneSched(dayOneSched => [...dayOneSched, 
-                        <div className='game-panel' key={gn}>
-                        <h2>Day {daysLeft+1}</h2>
-                        <h2><span>{team1.teamEmoji}<a style={{color: "#"+team1.teamColor}} href={'/team/'+team1.teamName}>{team1.teamName}</a></span> <br/>
-                        <a style={{color: "#"+team1.teamColor}} href={'/player/'+team1.players[daysLeft%team1.players.length].name}>{team1.players[daysLeft%team1.players.length].name}</a> <br/> 
-                        VS <br/>
-                        <span>{team2.teamEmoji}<a style={{color: "#"+team2.teamColor}} href={'/team/'+team2.teamName}>{team2.teamName}</a></span><br/>
-                        <a style={{color: "#"+team2.teamColor}} href={'/player/'+team2.players[daysLeft%team2.players.length].name}>{team2.players[daysLeft%team2.players.length].name}</a><br/></h2>
-                        <h2>
-                        {daysLeft >= 45 ?
-                        <div>
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="20px" className="weather" title="Null" alt='Null'></img> : ''}
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="20px" className="weather" title="Sunny" alt='Sunny'></img> : ''}
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 2 ? <img src="https://i.imgur.com/K3DWIqZ.png" height="20px" className="weather" title="Shuffle" alt='Shuffle'></img> : ''}
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 3 ? <img src="https://i.imgur.com/YDH7LQT.png" height="20px" className="weather" title="Waves" alt='Waves'></img> : ''}
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 4 ? <img src="https://i.imgur.com/FfuBnBD.png" height="20px" className="weather" title="Coffee" alt='Coffee'></img> : ''}
-                            { seasonData[0].postSeasonWeather[daysLeft-45][0+(gn/2)] === 5 ? <img src="https://i.imgur.com/NBMbLYO.png" height="20px" className="weather" title="Skipping" alt='Skipping'></img> : ''}
-                        </div>
-                        : 
-                        <div>
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="20px" className="weather" title="Null" alt='Null'></img> : ''}
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="20px" className="weather" title="Sunny" alt='Sunny'></img> : ''}
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 2 ? <img src="https://i.imgur.com/K3DWIqZ.png" height="20px" className="weather" title="Shuffle" alt='Shuffle'></img> : ''}
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 3 ? <img src="https://i.imgur.com/YDH7LQT.png" height="20px" className="weather" title="Waves" alt='Waves'></img> : ''}
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 4 ? <img src="https://i.imgur.com/FfuBnBD.png" height="20px" className="weather" title="Coffee" alt='Coffee'></img> : ''}
-                            { seasonData[0].weather[daysLeft][0+(gn/2)] === 5 ? <img src="https://i.imgur.com/NBMbLYO.png" height="20px" className="weather" title="Skipping" alt='Skipping'></img> : ''}
-                        </div>
-                        }
-                            
-                        <span style={{color: "#"+team1.teamColor}}>{Math.round((team1.gamesWon/(team1.gamesWon+team2.gamesWon))*80)+Math.round(((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)/((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)+(team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)))*20)}%</span> - <span style={{color: "#"+team2.teamColor}}>{Math.round((team2.gamesWon/(team2.gamesWon+team1.gamesWon))*80)+Math.round(((team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)/((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)+(team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)))*20)}%</span>
-                        <br/>
-                        <form>
-                            <button type="button" onClick={() => {handleClick(team1.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team1.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team1.teamEmoji}</button>{" "}
-                            <button type="button" onClick={() => {handleClick(team2.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team2.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team2.teamEmoji}</button>
-                        </form>
-                        </h2>
-                        </div>
-                    ])
+            if (seasonData[0].seasonDay < 45) {
+                for (let daysLeft = seasonData[0].seasonDay; daysLeft < seasonData[0].schedule.length; daysLeft++) {
+                    for (let gn = 0; gn < teamLength; gn += 2) {
+                        let team1 = seasonData[0].scheduleTeamInfo[seasonData[0].schedule[daysLeft][gn]]
+                        let team2 = seasonData[0].scheduleTeamInfo[seasonData[0].schedule[daysLeft][gn+1]]
+                        setDayOneSched(dayOneSched => [...dayOneSched, 
+                            <div className='game-panel' key={gn}>
+                            <h2>Day {daysLeft+1}</h2>
+                            <h2><span>{team1.teamEmoji}<a style={{color: "#"+team1.teamColor}} href={'/team/'+team1.teamName}>{team1.teamName}</a></span> <br/>
+                            <a style={{color: "#"+team1.teamColor}} href={'/player/'+team1.players[daysLeft%team1.players.length].name}>{team1.players[daysLeft%team1.players.length].name}</a> <br/> 
+                            VS <br/>
+                            <span>{team2.teamEmoji}<a style={{color: "#"+team2.teamColor}} href={'/team/'+team2.teamName}>{team2.teamName}</a></span><br/>
+                            <a style={{color: "#"+team2.teamColor}} href={'/player/'+team2.players[daysLeft%team2.players.length].name}>{team2.players[daysLeft%team2.players.length].name}</a><br/></h2>
+                            <h2>
+                            <div>
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="20px" className="weather" title="Null" alt='Null'></img> : ''}
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="20px" className="weather" title="Sunny" alt='Sunny'></img> : ''}
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 2 ? <img src="https://i.imgur.com/K3DWIqZ.png" height="20px" className="weather" title="Shuffle" alt='Shuffle'></img> : ''}
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 3 ? <img src="https://i.imgur.com/YDH7LQT.png" height="20px" className="weather" title="Waves" alt='Waves'></img> : ''}
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 4 ? <img src="https://i.imgur.com/FfuBnBD.png" height="20px" className="weather" title="Coffee" alt='Coffee'></img> : ''}
+                                { seasonData[0].weather[daysLeft][0+(gn/2)] === 5 ? <img src="https://i.imgur.com/NBMbLYO.png" height="20px" className="weather" title="Skipping" alt='Skipping'></img> : ''}
+                            </div>
+                                
+                            <span style={{color: "#"+team1.teamColor}}>{Math.round((team1.gamesWon/(team1.gamesWon+team2.gamesWon))*80)+Math.round(((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)/((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)+(team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)))*20)}%</span> - <span style={{color: "#"+team2.teamColor}}>{Math.round((team2.gamesWon/(team2.gamesWon+team1.gamesWon))*80)+Math.round(((team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)/((team1.players[daysLeft%team1.players.length].praying+team1.players[daysLeft%team1.players.length].publicity+team1.players[daysLeft%team1.players.length].pope)+(team2.players[daysLeft%team2.players.length].praying+team2.players[daysLeft%team2.players.length].publicity+team2.players[daysLeft%team2.players.length].pope)))*20)}%</span>
+                            <br/>
+                            <form>
+                                <button type="button" onClick={() => {handleClick(team1.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team1.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team1.teamEmoji}</button>{" "}
+                                <button type="button" onClick={() => {handleClick(team2.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team2.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team2.teamEmoji}</button>
+                            </form>
+                            </h2>
+                            </div>
+                        ])
+                    }
+                }
+            }
+            if (seasonData[0].seasonDay >= 45) {
+                for (let daysLeft = seasonData[0].seasonDay-45; daysLeft < seasonData[0].postSeasonSchedule.length; daysLeft++) {
+                    if (daysLeft >= 45) {if (seasonData[0].postSeasonSchedule[seasonData[0].daysLeft].length == 2) {teamLength = 2} else {teamLength = 4}}
+                    for (let gn = 0; gn < seasonData[0].postSeasonSchedule[daysLeft].length; gn += 2) {
+                        let team1 = seasonData[0].scheduleTeamInfo[seasonData[0].postSeasonSchedule[daysLeft][gn]]
+                        let team2 = seasonData[0].scheduleTeamInfo[seasonData[0].postSeasonSchedule[daysLeft][gn+1]]
+                        setDayOneSched(dayOneSched => [...dayOneSched, 
+                            <div className='game-panel' key={gn}>
+                            <h2>Postseason Day {daysLeft+46}</h2>
+                            <h2><span>{team1.teamEmoji}<a style={{color: "#"+team1.teamColor}} href={'/team/'+team1.teamName}>{team1.teamName}</a></span> <br/>
+                            <a style={{color: "#"+team1.teamColor}} href={'/player/'+team1.players[daysLeft+45%team1.players.length].name}>{team1.players[daysLeft+45%team1.players.length].name}</a> <br/> 
+                            VS <br/>
+                            <span>{team2.teamEmoji}<a style={{color: "#"+team2.teamColor}} href={'/team/'+team2.teamName}>{team2.teamName}</a></span><br/>
+                            <a style={{color: "#"+team2.teamColor}} href={'/player/'+team2.players[daysLeft+45%team2.players.length].name}>{team2.players[daysLeft+45%team2.players.length].name}</a><br/></h2>
+                            <h2>
+                            <div>
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 0 ? <img src="https://i.imgur.com/yvlrdat.png" height="20px" className="weather" title="Null" alt='Null'></img> : ''}
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 1 ? <img src="https://i.imgur.com/MX8RFc4.png" height="20px" className="weather" title="Sunny" alt='Sunny'></img> : ''}
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 2 ? <img src="https://i.imgur.com/K3DWIqZ.png" height="20px" className="weather" title="Shuffle" alt='Shuffle'></img> : ''}
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 3 ? <img src="https://i.imgur.com/YDH7LQT.png" height="20px" className="weather" title="Waves" alt='Waves'></img> : ''}
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 4 ? <img src="https://i.imgur.com/FfuBnBD.png" height="20px" className="weather" title="Coffee" alt='Coffee'></img> : ''}
+                                { seasonData[0].postSeasonWeather[daysLeft][0+(gn/2)] === 5 ? <img src="https://i.imgur.com/NBMbLYO.png" height="20px" className="weather" title="Skipping" alt='Skipping'></img> : ''}
+                            </div>
+                                
+                            <span style={{color: "#"+team1.teamColor}}>{Math.round((team1.gamesWon/(team1.gamesWon+team2.gamesWon))*80)+Math.round(((team1.players[daysLeft+45%team1.players.length].praying+team1.players[daysLeft+45%team1.players.length].publicity+team1.players[daysLeft+45%team1.players.length].pope)/((team1.players[daysLeft+45%team1.players.length].praying+team1.players[daysLeft+45%team1.players.length].publicity+team1.players[daysLeft+45%team1.players.length].pope)+(team2.players[daysLeft+45%team2.players.length].praying+team2.players[daysLeft+45%team2.players.length].publicity+team2.players[daysLeft+45%team2.players.length].pope)))*20)}%</span> - <span style={{color: "#"+team2.teamColor}}>{Math.round((team2.gamesWon/(team2.gamesWon+team1.gamesWon))*80)+Math.round(((team2.players[daysLeft+45%team2.players.length].praying+team2.players[daysLeft+45%team2.players.length].publicity+team2.players[daysLeft+45%team2.players.length].pope)/((team1.players[daysLeft+45%team1.players.length].praying+team1.players[daysLeft+45%team1.players.length].publicity+team1.players[daysLeft+45%team1.players.length].pope)+(team2.players[daysLeft+45%team2.players.length].praying+team2.players[daysLeft+45%team2.players.length].publicity+team2.players[daysLeft+45%team2.players.length].pope)))*20)}%</span>
+                            <br/>
+                            <form>
+                                <button type="button" onClick={() => {handleClick(team1.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team1.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team1.teamEmoji}</button>{" "}
+                                <button type="button" onClick={() => {handleClick(team2.teamName, seasonData[0].seasonDay+1, gn/2)}} style={{backgroundColor: "#"+team2.teamColor, border: "none", width: "80px", height: "25px", borderRadius: "25px"}}>Bet {team2.teamEmoji}</button>
+                            </form>
+                            </h2>
+                            </div>
+                        ])
+                    }
                 }
             }
             setDaysLoaded(true)
