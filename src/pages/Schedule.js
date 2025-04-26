1064453c2c9c5d4c61e6120a8290f162dd79a979
KetUnderscore <tannersock14@gmail.com>
2025-04-26 10:38:07 -0700

    STOP USING THE WRONG SEASON

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -18,7 +18,7 @@ function Schedule() {
     const [dayTwoSched, setDayTwoSched] = useState([])
     const [dayThreeSched, setDayThreeSched] = useState([])
 
-    const [betVal, setBetVal] = useState(10)
+    const [betVal, setBetVal] = useState(currentActiveSeason)
     const [teamVal, setTeamVal] = useState("")
 
     let userData = Cookies.get("userInfo")
