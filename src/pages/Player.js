7694ce821f7f3203ef354cf8b5262902908b98d3
KetUnderscore <tannersock14@gmail.com>
2025-08-29 00:48:42 -0700

    Season 12

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -12,7 +12,7 @@ function Player () {
     const [playerStatData, setPlayerStatData] = useState(null)
     const [playerLoaded, setPlayerLoaded] = useState(false)
     const [playerStatsLoaded, setPlayerStatsLoaded] = useState(false)
-    const [seasonNumber, setSeasonNumber] = useState(11) // Default is Season 11
+    const [seasonNumber, setSeasonNumber] = useState(12) // Default is Season 12
     const [seed, setSeed] = useState(1);
     
     let favoriteSeason
@@ -40,6 +40,7 @@ function Player () {
         return (
             <div className='season-form'>
                 <select value={seasonNumber} onChange={(e) => SeasonChanger(e.target.value)} className='season-select'>
+                    <option value="12">12</option>
                     <option value="11">11</option>
                     <option value="10">10</option>
                     <option value="9">9</option>
